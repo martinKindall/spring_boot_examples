884cfa5c58a55886d5d6e17c00d28afddcee7e73
Martin Cornejo <mart256@gmail.com>
2024-02-26 21:55:25 +0100

    Created scheduled job

---
+++ src/main/java/com/walruscode/examples/scheduling/ScheduleEx.java
@@ -0,0 +1,17 @@
+package com.walruscode.examples.scheduling;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableScheduling
+@Slf4j
+public class ScheduleEx {
+
+    @Scheduled(cron = "* */10 * * * *")
+    public void printMsg() {
+        log.info("executed scheduled job");
+    }
+}
