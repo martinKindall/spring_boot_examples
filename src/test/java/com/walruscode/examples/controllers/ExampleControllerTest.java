c6c5fc849661d96899e39bfa50373bdfcbf7dbe8
Martin Cornejo <mart256@gmail.com>
2024-02-29 20:54:10 +0100

    Added controller test

--- src/test/java/com/walruscode/examples/controllers/ExampleControllerTest.java
+++ src/test/java/com/walruscode/examples/controllers/ExampleControllerTest.java
@@ -1,5 +1,32 @@
 package com.walruscode.examples.controllers;
 
+import com.walruscode.examples.services.CarService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ExampleControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CarService carService;
+
+
+    @Test
+    public void testExampleController() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/example"))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.jsonPath("wheels").value(4))
+                .andExpect(MockMvcResultMatchers.jsonPath("brand").value("Audi"));
+    }
 }
